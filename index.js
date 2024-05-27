e04d470f39256cac769a22fb81c1cfbd41af3fb6
tasin000 <mdtasinanwar000@gmail.com>
2024-05-27 17:49:13 +0600

    Connected server to MongoDB

--- index.js
+++ index.js
@@ -11,6 +11,43 @@ app.get("/", async (req, res) => {
   res.send("Welcome to TechX Server!");
 });
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nwjyvfd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productsCollection = await client
+      .db("techXdb")
+      .collection("products");
+
+    app.get("/", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log("TechX Server is Running at port:", port);
 });
